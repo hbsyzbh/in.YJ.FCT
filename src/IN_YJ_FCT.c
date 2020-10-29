19920a8aca7e822d3f0e2939dcdfbb84744e0ecc
Bob.Zhu <zhubh@maxdone.com.cn>
2020-10-29 16:06:28 +0800

    UART OK   UART1 是仿真器 串口

--- src/IN_YJ_FCT.c
+++ src/IN_YJ_FCT.c
@@ -26,10 +26,10 @@ void main(void)
 		R_UART_Serial_Send("UART1\r\n", 7);
 		delay(10);
 		setD9(1);
-		R_UART_Serial_Send("UART2\r\n", 7);
+		R_UART2_Serial_Send("UART2\r\n", 7);
 		delay(10);
 		setD10(1);
-		R_UART_Serial_Send("UART3\r\n", 7);
+		R_UART3_Serial_Send("UART3\r\n", 7);
 		delay(10);
 		setD8(0);
 		setD9(0);
